119f93f1ef05c3fe405e98f1cbb77d03987a5283
Gustavo-HA <gustavoha40@gmail.com>
2023-02-22 07:45:16 -0600

    xd

--- Computacional/Tareas/T4/pi_cuadrado.f90
+++ Computacional/Tareas/T4/pi_cuadrado.f90
@@ -1,13 +1,13 @@
 program calculopicuadrado
     implicit none
     integer :: i, totales, dentro=0
-    real*8 :: x1, y1, xc = 0.5 , yc = 0.5, propuesta, distance
+    real*8 :: x1, y1, xc = 0.5d0 , yc = 0.5d0, propuesta, distance
     write(*,'(a)',advance = 'no')'Numero de iteraciones: ';read*,totales
 
     do i = 1,totales
         x1 = rand()
         y1 = rand()
-        distance = ((xc-x1)**2+(yc-y1)**2)**(1.0/2.0)
+        distance = ((xc-x1)**2.d0+(yc-y1)**2.d0)**(1.d0/2.d0)
         if (distance .lt. 0.5) dentro = dentro + 1
     enddo
 
--- Computacional/error.f90
+++ Computacional/error.f90
@@ -1,10 +1,15 @@
 program error
-    R = 0.00001
+    implicit none
+    REAL*8 :: R, f
     ! Se pierden digitos significativos al sumar a un número grande un pequeño
+    write(*,'(a)',advance='no')'Valor de x: ';read*,R
     print*,f(R)
 end
 
-function f(x)
-    f = (1-cos(x))/(x**2)
+real*8 function f(x)
+    implicit none 
+    real*8 :: x
+    f = x-sin(x)
+    !Se expande para que no aparezca el error cercano a 0
     return
 end
